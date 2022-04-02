25ff6c773354871cf9b8eb148e28e5f364c046d6
emir.vranac <vranacemir@gmail.com>
2022-04-02 16:44:54 +0200

    add react-bootstrap, create Header and Footer and add some sample data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
+
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to e-Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to e-Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
