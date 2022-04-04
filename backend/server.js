5a39438afa3c6d4b1fb350f0584afe027b87f14a
emir.vranac <vranacemir@gmail.com>
2022-04-04 21:51:36 +0200

    setup nodemon and concurrently, add env variables and params to dependency array

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require("express")
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
+
+dotenv.config()
 
 const app = express()
 
@@ -16,4 +19,10 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, () => console.log("Server is running on port 5000"))
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+  )
+)
