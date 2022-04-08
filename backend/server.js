9f9e9749a1b31fb27d230c3eb0b64042a4096209
emir.vranac <vranacemir@gmail.com>
2022-04-08 09:55:39 +0200

    add fetching products from db and custom error handlers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
-import products from "./data/products.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import productRouter from "./routes/productRoutes.js"
 
 dotenv.config()
 
@@ -10,17 +11,14 @@ const app = express()
 connectDB()
 
 app.get("/", (req, res) => {
-  res.send("API is running")
+  res.send("API running ...")
 })
 
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
+app.use("/api/products", productRouter)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
