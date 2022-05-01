c0c35ee31a76304d09b6785ef2053108ac38e60e
emir.vranac <vranacemir@gmail.com>
2022-05-01 21:23:37 +0200

    add update user profile functionality

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,6 +9,10 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_RESET,
 } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
@@ -39,7 +43,7 @@ export const userRegisterReducer = (state = {}, action) => {
   }
 }
 
-export const userDetailsReducer = (state = {}, action) => {
+export const userDetailsReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case USER_DETAILS_REQUEST:
       return { loading: true }
@@ -47,6 +51,21 @@ export const userDetailsReducer = (state = {}, action) => {
       return { loading: false, user: action.payload }
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const userUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, success: true, user: action.payload }
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_UPDATE_RESET:
+      return {}
     default:
       return state
   }
