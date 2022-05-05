13b519aaa16805dd5c7e7e456dd3747aef76bbb1
emir.vranac <vranacemir@gmail.com>
2022-05-05 10:18:14 +0200

    add update order paid status to order endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { addOrderItems, getOrderById } from "../controllers/orderControllers.js"
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderControllers.js"
 import protect from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", protect, addOrderItems)
 router.get("/:id", protect, getOrderById)
+router.get("/:id/pay", protect, updateOrderToPaid)
 
 export default router
