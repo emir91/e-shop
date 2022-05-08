90e81098370f47989a3fde40dd46ce8a2a5fc74b
emir.vranac <vranacemir@gmail.com>
2022-05-08 21:33:50 +0200

    add Admin User List

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -44,6 +44,16 @@ const Header = () => {
                   </Nav.Link>
                 </LinkContainer>
               )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userList">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productList">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
