823a68b665a477cc30df91250fe0812660411483
emir.vranac <vranacemir@gmail.com>
2022-05-08 20:12:39 +0200

    create getUsers endpoint and admin middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,15 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } from "../controllers/userControllers.js"
 import protect from "../middleware/authMiddleware.js"
+import admin from "../middleware/adminMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", registerUser)
+router.get("/", protect, admin, getUsers)
 router.post("/login", authUser)
 router.get("/profile", protect, getUserProfile)
 router.put("/profile", protect, updateUserProfile)
