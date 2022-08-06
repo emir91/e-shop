53c6cebffc842e14d557ec302563181e7e54602c
Emir Vranac <vranacemir@gmail.com>
2022-08-06 14:15:56 +0200

    list all orders (backend and frontend) - add omitted files

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,12 +4,15 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 } from "../controllers/orderControllers.js"
 import protect from "../middleware/authMiddleware.js"
+import admin from "../middleware/adminMiddleware.js"
 
 const router = express.Router()
 
 router.post("/", protect, addOrderItems)
+router.get("/", protect, admin, getOrders)
 router.get("/myorders", protect, getMyOrders)
 router.get("/:id", protect, getOrderById)
 router.put("/:id/pay", protect, updateOrderToPaid)
